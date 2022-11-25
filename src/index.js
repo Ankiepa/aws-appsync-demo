7fbc65362401421ef8cd20d29e5585982477cfbd
Aniket Patil <105452774+Ankiepa@users.noreply.github.com>
2022-11-25 17:22:19 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Client from 'aws-appsync'
+import {ApolloProvider} from 'react-apollo'
+import {Rehydrated } from 'aws-appsync-react'
+import AppSync from './Appync'
+
+const client = new Client({
+  url:AppSync.aws_appsync_graphqlEndpoint,
+  region:AppSync.aws_appsync_region,
+  auth:{
+    type:'API_KEY',
+    apiKey:AppSync.aws_appsync_apiKey
+  }
+})
+
+const WithProvider =()=>{
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WithProvider />
   </React.StrictMode>
 );
 
